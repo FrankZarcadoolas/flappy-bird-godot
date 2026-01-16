585c304563d1befd7b1e85d039449885814405fa
FrankZarc <fzarcadoolas@gmail.com>
2026-01-16 16:45:57 -0500

    trying to figure out jumping and collision

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
+const JUMP_VELOCITY = -300
+const GRAVITY = 500
+
+var velocity_y = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = Vector2(250, 300)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += Vector2(0, 1) * 50 * delta
+	pass
+	
+func _physics_process(delta: float) -> void:
+	velocity_y += GRAVITY * delta
+		
+	if Input.is_action_just_pressed("jump"):
+		velocity_y = JUMP_VELOCITY
+		
+	position.y += velocity_y * delta
